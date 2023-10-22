630124ec60f6333fab388e185621320cb652a7fe
schmonkli <eric@seervision.com>
2023-10-22 06:22:30 +0200

    initial commit

---
+++ expenseTrackerApp/src/App.js
@@ -0,0 +1,53 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "CAS Admission Fee",
+      amount: 9800.0,
+      date: new Date(2023, 7, 14),
+    },
+    { id: "e2", title: "Gravelbike", amount: 2510.8, date: new Date(2023, 0, 16) },
+    {
+      id: "e3",
+      title: "Groceries",
+      amount: 45.65,
+      date: new Date(2023, 9, 21),
+    },
+    {
+      id: "e4",
+      title: "IKEA furniture",
+      amount: 302,
+      date: new Date(2023, 9, 18),
+    },
+  ];
+
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
