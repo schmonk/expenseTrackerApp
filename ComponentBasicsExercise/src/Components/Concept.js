f662dbb1e1e252b164690ef599d809ff7ab61b54
schmonkli <eric@seervision.com>
2023-10-22 12:55:54 +0200

    use image prop

--- ComponentBasicsExercise/src/Components/Concept.js
+++ ComponentBasicsExercise/src/Components/Concept.js
@@ -2,7 +2,7 @@ const Concept = (props) => {
   const classes = `concept ${props.className}`;
   return (
     <li className={classes}>
-      <img src={props.imageSource} alt={props.concepts.title} />
+      <img src={props.concepts.image} alt={props.concepts.title} />
       <h2>{props.concepts.title}</h2>
       <p>{props.concepts.description}</p>
     </li>
