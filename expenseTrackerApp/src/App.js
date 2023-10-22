487fd9a1b8fcba685a37a51725b427b5f9f1879e
schmonkli <eric@seervision.com>
2023-10-22 10:51:56 +0200

    use JSX for app

--- expenseTrackerApp/src/App.js
+++ expenseTrackerApp/src/App.js
@@ -31,15 +31,15 @@ function App() {
   // 2nd arg: attributes of element as object
   // 3rd arg: content between tags of element
   // 4th + argument: whatever is deeper inside
-  return React.createElement("div", {}, React.createElement("h2", {}, "Let's get started!"),
-  React.createElement(Expenses, {items:expenses}));
+  // return React.createElement("div", {}, React.createElement("h2", {}, "Let's get started!"),
+  // React.createElement(Expenses, {items:expenses}));
 
-    // return (
-  //   <div>
-  //     <h2>Let's get started!</h2>
-  //     <Expenses items={expenses} />
-  //   </div>
-  // );
+    return (
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
+    </div>
+  );
 }
 
 export default App;
