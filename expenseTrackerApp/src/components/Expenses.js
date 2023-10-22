140b4935aee091bc510c786641b586ed98c18083
schmonkli <eric@seervision.com>
2023-10-22 07:06:08 +0200

    style assignment expenses

--- expenseTrackerApp/src/components/Expenses.js
+++ expenseTrackerApp/src/components/Expenses.js
@@ -1,8 +1,9 @@
+import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 
 function Expenses(props) {
   return (
-    <div>
+    <div className="expenses">
       <ExpenseItem
         title={props.expenses[0].title}
         amount={props.expenses[0].amount}
