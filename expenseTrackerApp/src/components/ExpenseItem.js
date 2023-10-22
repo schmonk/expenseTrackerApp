4ae302ef86e21dc23a72f61e582ae2d3e66c86ad
schmonkli <eric@seervision.com>
2023-10-22 06:42:09 +0200

    format expense date

--- expenseTrackerApp/src/components/ExpenseItem.js
+++ expenseTrackerApp/src/components/ExpenseItem.js
@@ -1,8 +1,16 @@
 import "./ExpenseItem.css";
 
 export default function ExpenseItem(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
   return (
     <div className="expense-item">
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div>{props.date.toDateString()}</div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
