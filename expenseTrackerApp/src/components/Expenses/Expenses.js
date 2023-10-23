67bbec5ad2bc88a9251ee8c9a816eb9400b0f05a
schmonkli <eric@seervision.com>
2023-10-23 20:19:38 +0200

    extract list display into separate component

--- expenseTrackerApp/src/components/Expenses/Expenses.js
+++ expenseTrackerApp/src/components/Expenses/Expenses.js
@@ -1,21 +1,12 @@
 import React, { useState } from "react";
 import "./Expenses.css";
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
 
-  let filterInfoText = "2019, 2021 & 2022";
-
-  if (filteredYear === "2019") {
-    filterInfoText = "2020, 2021 & 2022";
-  } else if (filteredYear === "2021") {
-    filterInfoText = "2019, 2020 & 2022";
-  } else if (filteredYear === "2022") {
-    filterInfoText = "2019, 2020 & 2021";
-  }
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
@@ -24,25 +15,11 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expensesContent = <p>No expenses found</p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
-
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {expensesContent}
-        <p>Data for years {filterInfoText} is hidden.</p>
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
