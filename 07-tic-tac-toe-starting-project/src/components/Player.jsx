f4a32677f36abe1304669c0df587da627bef34b2
schmonkli <eric@seervision.com>
2023-10-28 14:42:00 +0200

    tic tac toe finish

---
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+export default function Player({ initialName, symbol, isActive, onChangeName }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  return (
+    <li className={isActive ? 'active' : undefined}>
+      <span className='player'>
+        {isEditing && <input type='text' value={playerName} onChange={handleChange}></input>}
+        {!isEditing && <span className='player-name'>{playerName}</span>}
+        <span className='player-symbol'>{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+    </li>
+  );
+}
