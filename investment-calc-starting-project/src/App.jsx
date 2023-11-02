4fb32bee2be9d354949fd2ed4ca6eb520f43b87c
schmonkli <eric@seervision.com>
2023-11-02 17:05:36 +0100

    investment calc base

--- investment-calc-starting-project/src/App.jsx
+++ investment-calc-starting-project/src/App.jsx
@@ -1,7 +1,35 @@
+import Header from "./components/header.jsx";
+import Results from "./components/results.jsx";
+import UserInput from "./components/user-input.jsx";
+import { useState } from "react";
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const inputIsValid = userInput.duration >= 1;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((previousInput) => {
+      return {
+        ...previousInput,
+        [inputIdentifier]: Number(newValue),
+      };
+    });
+  }
+
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header />
+      <UserInput onChange={handleChange} userInput={userInput} />
+      {inputIsValid && <Results input={userInput} />}
+      {!inputIsValid && <p className="center">Please enter a duration greater than 0</p>}
+    </>
+  );
 }
 
-export default App
+export default App;
