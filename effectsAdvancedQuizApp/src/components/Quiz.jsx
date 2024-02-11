ecd7de51c67602a8bed483d7ed6eed70e9ec9b52
schmonkli <eric@seervision.com>
2024-02-11 17:46:47 +0100

    npm i, create base comps, import questions.js

---
+++ effectsAdvancedQuizApp/src/components/Quiz.jsx
@@ -0,0 +1,6 @@
+import { useState } from "react";
+export default function Quiz() {
+  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
+  const [userAnswers, setUserAnswersa] = useState([]);
+  return <p>Currently active question</p>;
+}
